3202cf8835d105b8f028f593f601939b48bcf884
Jordan Matelsky <j6k4m8@gmail.com>
2026-01-15 12:28:41 -0500

    feat: mem-optimize volume materialization with optional segment ID filtering

--- server/bossypaints/background.py
+++ server/bossypaints/background.py
@@ -1,3 +1,5 @@
+import os
+
 from zmesh import Mesher
 from intern import array as intern_array
 
@@ -8,6 +10,28 @@ from bossypaints.renderer import (
 )
 from bossypaints.tasks import JSONFileTaskQueueStore, Task, TaskID, TaskQueueStore
 from bossypaints.checkpoints import Checkpoint, JSONCheckpointStore
+
+def _mesh_batch_size(task: Task, total_ids: int) -> int:
+    env_size = os.getenv("BOSSYPAINTS_MESH_BATCH_SIZE")
+    if env_size:
+        try:
+            return max(1, int(env_size))
+        except ValueError:
+            pass
+
+    env_bytes = os.getenv("BOSSYPAINTS_MESH_BATCH_BYTES", str(1 << 30))
+    try:
+        target_bytes = max(1, int(env_bytes))
+    except ValueError:
+        target_bytes = 1 << 30
+
+    voxel_size = 1
+    x_size = int((task.x_max - task.x_min) / voxel_size)
+    y_size = int((task.y_max - task.y_min) / voxel_size)
+    z_size = int((task.z_max - task.z_min) / voxel_size)
+
+    channel_bytes = max(1, x_size * y_size * z_size * 8)
+    return max(1, min(total_ids, target_bytes // channel_bytes))
 
 def render_and_mesh(task_id: str, task: Task, checkpoints: list[Checkpoint], task_store: TaskQueueStore = None):
     try:
@@ -42,19 +66,27 @@ def render_and_mesh(task_id: str, task: Task, checkpoints: list[Checkpoint], tas
         # except Exception:
         #     pass
         mesher = Mesher((1, 1, 1))  # TODO: Get the resolution/voxel size from the task/source
-        vols = cv_renderer._materialize_xyz_volume(task, checkpoints, as_channels=True)
-        # vols = (x, y, z, C)
-        for c in range(vols.shape[-1]):
-            vol = vols[:, :, :, c]
-            mesher.mesh(vol, close=False)
-            for objid in mesher.ids():
-                mesh = mesher.get(
-                    objid,
-                    normals=False,
-                    # reduction_factor=10, max_error=2
-                )
-                with open(f"./exports/{task_id}/{objid}.obj", "wb") as f:
-                    f.write(mesh.to_obj())
+        segment_ids = sorted(set(poly.segmentID for checkpoint in checkpoints for poly in checkpoint.polygons))
+        if not segment_ids:
+            return
+
+        batch_size = _mesh_batch_size(task, len(segment_ids))
+        for offset in range(0, len(segment_ids), batch_size):
+            batch_ids = segment_ids[offset:offset + batch_size]
+            vols = cv_renderer._materialize_xyz_volume(task, checkpoints, as_channels=True, segment_ids=batch_ids)
+            # vols = (x, y, z, C)
+            for c in range(vols.shape[-1]):
+                vol = vols[:, :, :, c]
+                mesher.mesh(vol, close=False)
+                for objid in mesher.ids():
+                    mesh = mesher.get(
+                        objid,
+                        normals=False,
+                        # reduction_factor=10, max_error=2
+                    )
+                    with open(f"./exports/{task_id}/{objid}.obj", "wb") as f:
+                        f.write(mesh.to_obj())
+            del vols
 
     finally:
         # Always reset the export_pending flag when export completes (success or failure)
