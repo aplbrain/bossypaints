86623f881a00d1d5cd7dc312f80a17a4c045642a
Nicole Tregoning <nicole.tregoning@jhuapl.edu>
2026-03-02 11:51:12 -0500

    Save & Discard segment buttons (enter / delete alternatives for touch screen)

--- bossypaints/src/lib/AppWithTask.svelte
+++ bossypaints/src/lib/AppWithTask.svelte
@@ -22,8 +22,10 @@
 		HelpIcon,
 		LockIcon,
 		InfoIcon,
-		DownloadIcon
+		DownloadIcon,
+		CloseIcon
 	} from '$lib/icons';
+	import { discardSegment, saveSegment } from './utils/annotation';
 
 	const notyf = new Notyf();
 
@@ -393,6 +395,29 @@
 				<CheckIcon className="w-5 h-5" />
 			</button>
 		</div>
+
+		<!-- Discard / Save Segment Buttons - For Touch Screen -->
+		{#if nav.drawing && annotationStore.currentAnnotation}
+			<!-- add check for if drawing/done drawing?? -->
+			<div  class="fixed top-5 left-1/2 flex gap-2 z-30 pointer-events-auto">
+				<button
+					class="tooltip bg-gray-500 hover:bg-gray-600 text-white p-2 rounded-full shadow-lg transition-colors duration-200"
+					aria-label="Discard Segment"
+					data-tooltip="Discard Segment"
+					onclick={() => discardSegment(annotationStore, nav.layer)}
+				>
+					<CloseIcon class="w-3 h-3" />
+				</button>
+				<button
+					class="tooltip bg-green-500 hover:bg-green-600 text-white p-2 rounded-full shadow-lg transition-colors duration-200"
+					aria-label="Save Segment"
+					data-tooltip="Save Segment"
+					onclick={() => saveSegment(annotationStore, nav.layer)}
+				>
+					<CheckIcon className="w-3 h-3" />
+				</button>
+			</div>
+		{/if}
 	{/if}
 
 	<!-- Navigation Confirmation Modal -->
